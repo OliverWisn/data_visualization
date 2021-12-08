93793b5cc42063d44b9f328744a22cf31c7bd4e6
Olgierd Wisniewski <79323713+OliverWisn@users.noreply.github.com>
2021-12-08 16:41:06 +0100

    Correct the data points for the x axis
    The data points for the x axis were corrected. They were displayed
    from 0, not 1.

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 
 fig, ax = plt.subplots()
-ax.plot(squares, linewidth=3)
+ax.plot(input_values, squares, linewidth=3)
 
 # Defining of the chart title and axis labels.
 ax.set_title("Squares of the numbers", fontsize=24)
