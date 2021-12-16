7887c64e486f7e61bc669cf9fb0ec627e962bf63
Olgierd Wisniewski <79323713+OliverWisn@users.noreply.github.com>
2021-12-16 16:12:05 +0100

    Add a chart style and the descriptions
    Adding a random chart style and the descriptions in the code of
    the script mpl_squares.py.

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,8 +1,13 @@
 import matplotlib.pyplot as plt
 
+# Input values for the x and y axes.
 input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 
+# Setting of the chart style.
+plt.style.use("Solarize_Light2")
+
+# Generating of the chart.
 fig, ax = plt.subplots()
 ax.plot(input_values, squares, linewidth=3)
 
@@ -14,4 +19,5 @@ ax.set_ylabel("Squares of the values", fontsize=14)
 # Defining of the size of the labels.
 ax.tick_params(axis="both", labelsize=14)
 
+# Chart display.
 plt.show()
