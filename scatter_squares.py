14ab4aeac4c42115801a23a2edd089c51975f896
Olgierd Wisniewski <79323713+OliverWisn@users.noreply.github.com>
2021-12-17 09:09:56 +0100

    Calculate the data automatically
    The data is calculated automatically by the program. Additionally,
    ranges have been defined for each axis.

--- scatter_squares.py
+++ scatter_squares.py
@@ -4,15 +4,15 @@
 import matplotlib.pyplot as plt
 
 # Input values for the x and y axes.
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 
 # Setting of the chart style.
 plt.style.use("ggplot")
 
 # Generating of the chart.
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values, s=10)
 
 # Defining of the chart title and axis labels.
 ax.set_title("Squares of the numbers", fontsize=24)
@@ -21,6 +21,9 @@ ax.set_ylabel("Squares of the values", fontsize=14)
 
 # Defining of the size of the labels.
 ax.tick_params(axis="both", which="major", labelsize=14)
+
+# Defining the range for each axis.
+ax.axis([0, 1100, 0, 1100000])
 
 # Chart display.
 plt.show()
