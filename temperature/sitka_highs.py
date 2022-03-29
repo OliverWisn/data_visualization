e9615dc65b790a5ba5f133289f6a17107062cddb
Olgierd Wisniewski <79323713+OliverWisn@users.noreply.github.com>
2022-03-29 11:42:04 +0200

    Display data in a temperature graph
    Data display on the temperature graph has been added.

--- temperature/sitka_highs.py
+++ temperature/sitka_highs.py
@@ -3,6 +3,8 @@
 
 import csv
 
+import matplotlib.pyplot as plt
+
 filename =  "data/sitka_weather_07-2018_simple.csv"
 with open(filename) as f:
     reader = csv.reader(f)
@@ -14,4 +16,16 @@ with open(filename) as f:
         high = int(row[5])
         highs.append(high)
 
-    print(highs)
+# Plot data.
+plt.style.use("seaborn")
+fig, ax = plt.subplots()
+ax.plot(highs, c="red")
+
+# Format the plot.
+ax.set_title("Highest temperature of the day, July 2018", fontsize=24)
+ax.set_xlabel("", fontsize=16)
+ax.set_ylabel("Temperature (F)", fontsize=16)
+ax.tick_params(axis="both", which="major", labelsize=16)
+
+# Plot display.
+plt.show()
