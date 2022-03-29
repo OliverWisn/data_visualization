03a45d2dcfcda739499db4859ac803791cf122ec
Olgierd Wisniewski <79323713+OliverWisn@users.noreply.github.com>
2022-03-29 12:28:26 +0200

    Displaying the dates in the plot
    The code for the loading of the dates and the displaying it in
    the plot has been added.

--- temperature/sitka_highs.py
+++ temperature/sitka_highs.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import csv
+from datetime import datetime
 
 import matplotlib.pyplot as plt
 
@@ -10,20 +11,24 @@ with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    # Downloading of the maximum temperatures from the file
-    highs = []
+    # Downloading of the dates and the maximum temperatures from 
+    # the file.
+    dates, highs = [], []
     for row in reader:
+        current_date = datetime.strptime(row[2], "%Y-%m-%d")
         high = int(row[5])
+        dates.append(current_date)
         highs.append(high)
 
-# Plot data.
+# Generating the graph of the highest temperatures.
 plt.style.use("seaborn")
 fig, ax = plt.subplots()
-ax.plot(highs, c="red")
+ax.plot(dates, highs, c="red")
 
 # Format the plot.
 ax.set_title("Highest temperature of the day, July 2018", fontsize=24)
 ax.set_xlabel("", fontsize=16)
+fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize=16)
 ax.tick_params(axis="both", which="major", labelsize=16)
 
