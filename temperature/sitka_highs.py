9878220120094e209c00b7f0f5ddefa147a9f8df
Olgierd Wisniewski <79323713+OliverWisn@users.noreply.github.com>
2022-03-29 11:14:46 +0200

    Add a code to read the highest temperatures
    Added the code to read the highest temperatures from the csv file.

--- temperature/sitka_highs.py
+++ temperature/sitka_highs.py
@@ -8,5 +8,10 @@ with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    for index, column_header in enumerate(header_row):
-        print(index, column_header)
+    # Downloading of the maximum temperatures from the file
+    highs = []
+    for row in reader:
+        high = int(row[5])
+        highs.append(high)
+
+    print(highs)
