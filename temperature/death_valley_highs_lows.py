bee6ff4142f637f80ebbf3c9828fa299b93a1628
Olgierd Wisniewski <79323713+OliverWisn@users.noreply.github.com>
2022-04-26 12:32:42 +0200

    Handle new data and a error
    By the changing of the code, a new graph is created for the new data.
    The simple error handling has also been added.

--- temperature/death_valley_highs_lows.py
+++ temperature/death_valley_highs_lows.py
@@ -11,33 +11,35 @@ with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    for index, column_header in enumerate(header_row):
-        print(index, column_header)
+    # Downloading of the dates and the maximum and minimum temperatures 
+    # from the file.
+    dates, highs, lows = [], [], []
+    for row in reader:
+        current_date = datetime.strptime(row[2], "%Y-%m-%d")
+        try:
+            high = int(row[4])
+            low = int(row[5])
+        except ValueError:
+            print(f"No data for {current_date}.")
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
-#     # Downloading of the dates and the maximum and minimum temperatures 
-#     # from the file.
-#     dates, highs, lows = [], [], []
-#     for row in reader:
-#         current_date = datetime.strptime(row[2], "%Y-%m-%d")
-#         high = int(row[5])
-#         low = int(row[6])
-#         dates.append(current_date)
-#         highs.append(high)
-#         lows.append(low)
+# Generating the plot of the highest temperatures.
+plt.style.use("seaborn")
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c="red", alpha=0.5)
+ax.plot(dates, lows, c="blue", alpha=0.5)
+ax.fill_between(dates, highs, lows, facecolor="blue", alpha=0.1)
 
-# # Generating the plot of the highest temperatures.
-# plt.style.use("seaborn")
-# fig, ax = plt.subplots()
-# ax.plot(dates, highs, c="red", alpha=0.5)
-# ax.plot(dates, lows, c="blue", alpha=0.5)
-# ax.fill_between(dates, highs, lows, facecolor="blue", alpha=0.1)
+# Format the plot.
+ax.set_title("Highest and lowest temperature of the day - 2018\nDeath Valley, \
+    California", fontsize=20)
+ax.set_xlabel("", fontsize=16)
+fig.autofmt_xdate()
+ax.set_ylabel("Temperature (F)", fontsize=16)
+ax.tick_params(axis="both", which="major", labelsize=16)
 
-# # Format the plot.
-# ax.set_title("Highest and lowest temperature of the day - 2018", fontsize=24)
-# ax.set_xlabel("", fontsize=16)
-# fig.autofmt_xdate()
-# ax.set_ylabel("Temperature (F)", fontsize=16)
-# ax.tick_params(axis="both", which="major", labelsize=16)
-
-# # Plot display.
-# plt.show()
+# Plot display.
+plt.show()
