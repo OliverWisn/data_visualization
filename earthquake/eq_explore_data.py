8194e123988a601ea6ef9a2720f5227d6aa0a99b
Olgierd Wisniewski <79323713+OliverWisn@users.noreply.github.com>
2022-04-26 17:06:29 +0200

    Add a new script
    Converts a JSON file to another JSON file with a more human-readable
    structure.

---
+++ earthquake/eq_explore_data.py
@@ -0,0 +1,12 @@
+# eq_explore_data.py
+# -*- coding: utf-8 -*-
+
+import json
+
+# Data structure analysis.
+filename =  'data/eq_data_1_day_m1.json'
+with open(filename) as f:
+    all_eq_data = json.load(f)
+readable_file = 'data/readable_eq_data.json'
+with open(readable_file, 'w') as f:
+    json.dump(all_eq_data, f, indent=4)
