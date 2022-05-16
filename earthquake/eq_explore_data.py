550cd34a874b856b262a60d8e78e91a0748be8a9
Olgierd Wisniewski <79323713+OliverWisn@users.noreply.github.com>
2022-05-16 16:15:24 +0200

    Extract the data about the location.
    The data about the force and the location of the earthquake are now
    extracted from the json file. Lists are made of them.

--- earthquake/eq_explore_data.py
+++ earthquake/eq_explore_data.py
@@ -11,10 +11,17 @@ with open(filename) as f:
 # Creation of the earthquake list.
 all_eq_dicts = all_eq_data['features']
 
-# Mining of the force of the earthquake.
-mags = []
+# Extracting of the data about the force and the location of 
+# the earthquake.
+mags, lons, lats = [], [], []
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
     mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
 
-print(mags[:20])
+print(mags[:10])
+print(lons[:5])
+print(lats[:5])
