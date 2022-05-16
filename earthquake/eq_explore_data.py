aaeb7fd68710d0b4f42f81932a39d85787a5b6b3
Olgierd Wisniewski <79323713+OliverWisn@users.noreply.github.com>
2022-05-16 15:49:43 +0200

    Mine of the force of the earthquake
    The list with the earthquake forces is created.

--- earthquake/eq_explore_data.py
+++ earthquake/eq_explore_data.py
@@ -8,5 +8,13 @@ filename =  'data/eq_data_1_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
 
+# Creation of the earthquake list.
 all_eq_dicts = all_eq_data['features']
-print(len(all_eq_dicts))
+
+# Mining of the force of the earthquake.
+mags = []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    mags.append(mag)
+
+print(mags[:20])
