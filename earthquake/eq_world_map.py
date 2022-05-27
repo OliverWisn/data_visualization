29773c7d89824ceec88cfff51698b757da46f526
Olgierd Wisniewski <79323713+OliverWisn@users.noreply.github.com>
2022-05-27 14:06:31 +0200

    Add a text displayed for each point
    Added the text displayed after the selecting a point on the map.

--- earthquake/eq_world_map.py
+++ earthquake/eq_world_map.py
@@ -16,20 +16,23 @@ all_eq_dicts = all_eq_data['features']
 
 # Extracting of the data about the force and the location of 
 # the earthquake.
-mags, lons, lats = [], [], []
+mags, lons, lats, hover_texts = [], [], [], []
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
     lon = eq_dict['geometry']['coordinates'][0]
     lat = eq_dict['geometry']['coordinates'][1]
+    title = eq_dict['properties']['title']
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
+    hover_texts.append(title)
 
 # Earthquake map.
 data = [{
     'type': 'scattergeo', 
     'lon': lons, 
-    'lat': lats, 
+    'lat': lats,
+    'text': hover_texts, 
     'marker': {
         'size': [5*mag for mag in mags], 
         'color': mags, 
