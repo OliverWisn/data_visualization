2c3ad3e9ce1f72d282aa8c3cc903f180b8ea368a
Olgierd Wisniewski <79323713+OliverWisn@users.noreply.github.com>
2022-06-20 17:05:19 +0200

    Process the API response
    The API response can now be processed.

--- github_repos_visualization/python_repos.py
+++ github_repos_visualization/python_repos.py
@@ -1,1 +1,16 @@
 # python_repos.py
+
+import requests
+
+# Make the API call and save the response received.
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+headers = {'Accept': 'aplication/vnd.github.v3+json'}
+r = requests.get(url, headers=headers)
+print(f'Kod stanu: {r.status_code}')
+print(url)
+
+# Putting the API response in a variable
+response_dict = r.json()
+
+# Processing the results.
+print(response_dict.keys())
