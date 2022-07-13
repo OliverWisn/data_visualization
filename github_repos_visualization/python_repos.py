b1c60d7422a55ff4ec962ab57aff1ed2b77e0290
Olgierd Wisniewski <79323713+OliverWisn@users.noreply.github.com>
2022-07-13 16:16:13 +0200

    Print the repositories with the most stars
    By the way, I had to add the function to handle characters not
    displayed in the python shell in windows. Without this function,
    an error is generated.

--- github_repos_visualization/python_repos.py
+++ github_repos_visualization/python_repos.py
@@ -1,6 +1,20 @@
 # python_repos.py
 
+import sys
+
 import requests
+
+def uprint(*objects, sep=' ', end='\n', file=sys.stdout):
+	"""
+	Replacing of the problematic characters with the replacement 
+	characters.
+	"""
+	enc = file.encoding
+	if enc == 'UTF-8':
+		print(*objects, sep=sep, end=end, file=file)
+	else:
+		f = lambda obj: str(obj).encode(enc, errors='backslashreplace').decode(enc)
+		print(*map(f, objects), sep=sep, end=end, file=file)
 
 # Make the API call and save the response received.
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
@@ -16,14 +30,10 @@ print(f"Total count of the repositories: {response_dict['total_count']}")
 repo_dicts = response_dict['items']
 print(f'Count of the repositories returned: {len(repo_dicts)}')
 
-# Analysis of the first repository.
-repo_dict = repo_dicts[0]
-
-print('\nSelected information about the first repository:')
-print(f"Name: {repo_dict['name']}")
-print(f"Owner: {repo_dict['owner']['login']}")
-print(f"Stargazers: {repo_dict['stargazers_count']}")
-print(f"Repository: {repo_dict['html_url']}")
-print(f"Created at: {repo_dict['created_at']}")
-print(f"Updated at: {repo_dict['updated_at']}")
-print(f"Description: {repo_dict['description']}")
+print('\nSelected information about each repository:')
+for repo_dict in repo_dicts:
+	print(f"\nName: {repo_dict['name']}")
+	print(f"Owner: {repo_dict['owner']['login']}")
+	print(f"Stargazers: {repo_dict['stargazers_count']}")
+	print(f"Repository: {repo_dict['html_url']}")
+	uprint(f"Description: {repo_dict['description']}")
