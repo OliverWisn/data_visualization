1f4a5e4e9b1dce01c7ba8f648f468e74dd0cb00c
Olgierd Wisniewski <79323713+OliverWisn@users.noreply.github.com>
2022-07-13 14:19:42 +0200

    Analyze the information from the API
    The analysis was needed of what information is provided from the API.

--- github_repos_visualization/python_repos.py
+++ github_repos_visualization/python_repos.py
@@ -7,10 +7,17 @@ url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 headers = {'Accept': 'aplication/vnd.github.v3+json'}
 r = requests.get(url, headers=headers)
 print(f'Status code: {r.status_code}')
-print(url)
 
-# Putting the API response in a variable
+# Putting the API response in a variable.
 response_dict = r.json()
+print(f"Total count of the repositories: {response_dict['total_count']}")
 
-# Processing the results.
-print(response_dict.keys())
+# Processing info about the repositories.
+repo_dicts = response_dict['items']
+print(f'Count of the repositories returned: {len(repo_dicts)}')
+
+# Analysis of the first repository.
+repo_dict = repo_dicts[0]
+print(f'\nKeys: {len(repo_dict)}')
+for key in sorted(repo_dict.keys()):
+	print(key)
